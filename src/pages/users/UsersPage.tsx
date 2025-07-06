6097866d02d0b4c85449f8cac670cfd829bbad8d
MSAHARSHREDDY <saharshreddym59@gmail.com>
2025-07-06 06:16:19 +0000

    add creating user function

--- src/pages/users/UsersPage.tsx
+++ src/pages/users/UsersPage.tsx
@@ -1,9 +1,9 @@
 import { Breadcrumb, Button, Drawer, Form, Space, Table, theme } from "antd";
 import { RightOutlined } from "@ant-design/icons";
 import { Link, Navigate } from "react-router-dom";
-import { getUsers } from "../../http/Api";
-import { useQuery } from "@tanstack/react-query";
-import type { User } from "../../types";
+import { createUser, getUsers } from "../../http/Api";
+import { QueryClient, useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import type { CreateUserData, User } from "../../types";
 import { useAuthStore } from "../../Store";
 import UsersFilter from "./UsersFilter";
 import {PlusOutlined} from "@ant-design/icons"
@@ -51,7 +51,8 @@ const columnsData=[
 ]
 
 const UsersPage = () => {
-  
+  const [form]=Form.useForm()
+  const queryClient=useQueryClient()
   const [currentEditingUser, setCurrentEditingUser] = React.useState<User | null>(null);
   const{
     token:{colorBgLayout},
@@ -69,6 +70,24 @@ const UsersPage = () => {
   });
 
   const {user}=useAuthStore()
+
+  const {mutate:userMutate}=useMutation({
+    mutationKey:["user"],
+    mutationFn:async(data:CreateUserData)=>createUser(data).then((res)=>res.data),
+    onSuccess:async()=>{
+       queryClient.invalidateQueries({ queryKey: ['users'] });
+      return
+    }
+  })
+  const onHandleSubmit=async()=>{
+    await form.validateFields()
+      console.log("Form values",form.getFieldsValue())
+    await userMutate(form.getFieldsValue())
+    form.resetFields()//once you submit it clear all the values
+    setDrawerOpen(false)//It helps you to close the drawer
+  
+  }
+
   if(user?.role!=="admin"){//we are providing protecting routes from url is user is not a admin, if user is not admin it tries to redirect to the home page
     return <Navigate to="/" replace={true}></Navigate>
   }
@@ -99,17 +118,23 @@ const UsersPage = () => {
       <Table  columns={columnsData} dataSource={users} rowKey={"id"}/>
 
       {/**It is used for displaying drawer */}
-      <Drawer title="Create User" styles={{body:{background:colorBgLayout}}} width={720} destroyOnClose={true} open={drawerOpen} onClose={()=>{
+      <Drawer title="Create User" styles={{body:{background:colorBgLayout}}} width={720} destroyOnClose={true} open={drawerOpen} 
+      onClose={()=>{
+        form.resetFields()//once you press on "x" it clear all the values
         setDrawerOpen(false)
         console.log("closing")
-      }} extra={
+      }} 
+      extra={
         <Space>
-            <Button>Cancel</Button>
-            <Button type="primary">Submit</Button>
+            <Button onClick={()=>{
+            form.resetFields()//once you cancel it clear all the values
+            setDrawerOpen(false)
+            }}>Cancel</Button>
+            <Button type="primary" onClick={onHandleSubmit}>Submit</Button>
         </Space>
       }>
         {/**Creating a user formcomponent */}
-        <Form layout="vertical">
+        <Form layout="vertical" form={form}>
           <UserForm isEditMode={!!currentEditingUser} />
         </Form>
         
