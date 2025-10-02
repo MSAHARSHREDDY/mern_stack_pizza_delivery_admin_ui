d6b025e3a076321ce0f82b5e0967b1bdaa2312ba
MSAHARSHREDDY <saharshreddym59@gmail.com>
2025-10-02 21:09:27 +0530

    add tsconfig

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+// https://vite.dev/config/
+export default defineConfig({
+    server: {
+        watch: {
+            usePolling: true
+        }
+    },
+    plugins: [react()],
+    test: {
+        environment: "jsdom",
+        setupFiles: './setupTest.ts',
+        globals: true,
+    }
+});
